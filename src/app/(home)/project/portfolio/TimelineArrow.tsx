fa92e96c755acd188ac463277b30697270d13880
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2025-11-14 18:46:11 -0500

    have all the elements for porfolio page

---
+++ src/app/(home)/project/portfolio/TimelineArrow.tsx
@@ -0,0 +1,19 @@
+export default function TimelineArrow() {
+  return (
+    <svg
+      width="1136"
+      height="24"
+      viewBox="0 0 1136 24"
+      className="w-4/5 m-auto"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M-0.000130653 11.547L11.5469 23.094L23.0939 11.547L11.5469 -8.58307e-06L-0.000130653 11.547ZM1135.55 11.547L1115.55 -8.58307e-06V23.094L1135.55 11.547ZM11.5469 11.547V13.547H1117.55V11.547V9.547H11.5469V11.547Z"
+        fill="#286CE4"
+        fill-opacity="0.5"
+      />
+    </svg>
+  );
+}
+
--- src/app/(home)/project/portfolio/page.tsx
+++ src/app/(home)/project/portfolio/page.tsx
@@ -1,14 +1,43 @@
 import Image from "next/image";
+import SmallChevron from "./SmallChevron";
+import TimelineArrow from "./TimelineArrow";
 
 export default function PortfolioPage() {
   return (
-    <div>
-      <Image src="/" alt="" width={0} height={0} />
+    <div className="w-4/5 m-auto mt-24">
+      <div className="flex">
+        <Image className="basis-0 grow" src="/" alt="" width={0} height={0} />
+        <div className="basis-0 grow">
+          <h1>Portfolio</h1>
+          <p>
+            Here&apos;s all you need to know about this website, from its design
+            process to its deployment, passing of course through the building
+            process.
+          </p>
+          <h2>Technologies</h2>
+          <div></div>
+        </div>
+      </div>
       <div>
-        <h1>Portfolio</h1>
-        <p>Here&apos;s all you need to know about this website.</p>
-        <h2>Collaborators</h2>
-        <p>Me, Myself, and I</p>
+        <header className="flex justify-between w-full">
+          <h2>Timeline</h2>
+          <button className="flex">
+            View Full Timeline <SmallChevron />
+          </button>
+        </header>
+        <TimelineArrow />
+      </div>
+      <div>
+        <header className="flex justify-between">
+          <h2>Challenges</h2>
+          <h2>Solutions</h2>
+        </header>
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+      <div>
+        <h2>Future Plans</h2>
       </div>
     </div>
   );
