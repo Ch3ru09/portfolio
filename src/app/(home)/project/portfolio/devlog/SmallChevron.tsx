205fb68ca4aaff7a66bb718cb4bb62e62c0b549f
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2025-11-17 01:55:59 -0500

    added styles to devlog

--- src/app/(home)/project/portfolio/devlog/SmallChevron.tsx
+++ src/app/(home)/project/portfolio/devlog/SmallChevron.tsx
@@ -6,6 +6,7 @@ export default function SmallChevron() {
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      className="-rotate-90"
     >
       <path
         d="M6 9L12 15L18 9"
--- src/app/(home)/project/portfolio/devlog/page.tsx
+++ src/app/(home)/project/portfolio/devlog/page.tsx
@@ -1,14 +1,46 @@
 import Logs from "@/assets/portfolio/devlog.json";
+import SmallChevron from "./SmallChevron";
 
 export default function Devlog() {
   return (
-    <ul>
+    <ul className="flex flex-col gap-16 mt-12 w-1/2 mb-12">
       {Logs.map((log, i) => {
         return (
-          <li key={i}>
-            <p>Day {log.day}</p>
-            <h2>{log.title}</h2>
-            <p>{log.log}</p>
+          <li key={i} className="flex">
+            <div>
+              <SmallChevron />
+            </div>
+            <div>
+              <div className="flex text-primary text-4xl font-heading font-bold">
+                <p>Day {log.day} — </p> &nbsp;
+                <h2>{log.title}</h2>
+              </div>
+              <p className="ml-4 mt-2">{log.log}</p>
+              {log.todos.length > 0 && (
+                <>
+                  <h3 className="mt-6 text-primary text-2xl font-heading font-bold">
+                    Todo Tomorrow
+                  </h3>
+                  <ul className="ml-4 mt-2">
+                    {log.todos.map((todo, j) => {
+                      return <li key={j}>{todo}</li>;
+                    })}
+                  </ul>
+                </>
+              )}
+              {log.is_done.length > 0 && (
+                <>
+                  <h3 className="mt-6 text-primary text-2xl font-heading font-bold">
+                    Done Today
+                  </h3>
+                  <ul className="ml-4 mt-2">
+                    {log.is_done.map((todo, j) => {
+                      return <li key={j}>{todo}</li>;
+                    })}
+                  </ul>
+                </>
+              )}
+            </div>
           </li>
         );
       })}
